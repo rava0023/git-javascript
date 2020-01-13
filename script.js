7ce8b73570a5879b7c07f315a0e15c86e37b7e7d
Parth Raval <rava0023@algonquinlive.com>
2020-01-13 17:05:12 -0500

    Added all files from previous repository

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = 'Parth'
+console.log(`Hello, my name is ${name},`)
+
+let number = 5
+let product =  number * 2
+console.log(`The number ${number} multiplied b 2 is ${number * 2}.`)
+
+console.log(`The number Multiplied by 5 is ${product *5}.`)
+console.log(`The result devided by 5 is ${(product * 5) / number}.`)
+let finalvalue = (product * 5) / number
+Console.log(`The result minus 7 is ${finalvalue - 7}.`)
